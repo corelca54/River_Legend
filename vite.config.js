247f7c1a7a57846a5f9774c87bbdaa1e206111c6
corelca54 <118087457+corelca54@users.noreply.github.com>
2025-09-29 02:30:33 +0000

    Port

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Permite conexiones desde cualquier IP
-    port: 5173,
-    strictPort: true
-  }
+  // El servidor usará el puerto automático por defecto
 })
